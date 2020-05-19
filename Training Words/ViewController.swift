36f0add1e169af1f13d6e2d0154c472e36e34312
Nikita <justnicke@gmail.com>
2020-05-19 17:22:23 +0300

    Initial Commit

---
+++ Training Words/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Training Words
+//
+//  Created by Nikita Sukachev on 19.05.2020.
+//  Copyright © 2020 Nikita Sukachev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
